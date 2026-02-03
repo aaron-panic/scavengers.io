90d1f96de4f6a4e88a551dc9fe3dae0fb92d7e49
Aaron Reichenbach <aaron@scavengers.io>
2026-02-02 22:19:15 -0700

    licensing

--- site/blueprints/admin.py
+++ site/blueprints/admin.py
@@ -1,3 +1,19 @@
+# admin.py - Routing blueprint for /admin (administration)
+# Copyright (C) 2026 Aaron Reichenbach
+#
+# This program is free software: you can redistribute it and/or modify         
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import secrets
 import string
 from flask import Blueprint, render_template, flash, redirect, url_for
--- site/middleware.py
+++ site/middleware.py
@@ -1,3 +1,19 @@
+# middleware.py - RBAC functions
+# Copyright (C) 2026 Aaron Reichenbach
+#
+# This program is free software: you can redistribute it and/or modify         
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from flask import session, redirect, url_for, render_template
 
 def check_access(allowed_roles):
